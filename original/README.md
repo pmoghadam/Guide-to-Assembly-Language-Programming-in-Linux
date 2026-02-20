80890da6d311fdf33d70f01eeacfb414f188616b
Pejman Moghadam <pmoghadam@yahoo.com>
2026-02-20 21:46:34 +0330

    PutCh added to original

---
+++ original/README.md
@@ -0,0 +1,16 @@
+# The original implementation from the book
+
+
+```
+$ nm io.o | grep " T "
+00000089 T proc_GetCh
+00000111 T proc_GetInt
+0000022c T proc_GetLInt
+00000055 T proc_GetStr
+00000000 T proc_nwln
+00000019 T proc_PutCh
+000000bb T proc_PutInt
+000001d4 T proc_PutLInt
+00000037 T proc_PutStr
+```
+
---
+++ original/assemble.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+
+[ "$#" != "1" ] && exit
+
+ASSEMBLY="$1"
+BASENAME="$(basename ${ASSEMBLY} .nasm)"
+BASENAME="$(basename ${BASENAME} .asm)"
+BASENAME="$(basename ${BASENAME} .s)"
+
+set -e
+set -x
+
+nasm -f elf      "${ASSEMBLY}"        -o "${BASENAME}".o
+ld   -m elf_i386 "${BASENAME}".o io.o -o "${BASENAME}".out
+rm "${BASENAME}".o
+
