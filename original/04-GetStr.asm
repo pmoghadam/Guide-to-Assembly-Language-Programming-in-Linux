ad76bd617df12a611f7d7b500274572d1fd4ede2
Pejman Moghadam <pmoghadam@yahoo.com>
2026-02-24 00:51:22 +0330

    GetStr 32bit implementation

---
+++ original/04-GetStr.asm
@@ -0,0 +1,20 @@
+%include "io.mac"
+
+.UDATA
+	name resb 50
+
+.DATA
+
+	msg db "What is yourname? ", 0
+	hello db "Hello ", 0
+
+.CODE
+	.STARTUP
+
+	PutStr msg
+	GetStr name, 50
+	PutStr hello
+	PutStr name
+	nwln
+
+	.EXIT
